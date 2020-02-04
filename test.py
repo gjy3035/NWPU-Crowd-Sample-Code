cf24007a7d796fb759b0352e23b11b6e40b6dcdc
Junyu Gao <gjy3035@gmail.com>
2020-02-04 10:10:50 +0800

    fix bugs for test.py

--- test.py
+++ test.py
@@ -14,11 +14,14 @@ from config import cfg
 from misc.utils import *
 import scipy.io as sio
 from PIL import Image, ImageOps
+import sys
+
+#import quailitycc
 
 torch.cuda.set_device(0)
 torch.backends.cudnn.benchmark = True
 
-mean_std = ([0.452016860247, 0.447249650955, 0.431981861591],[0.23242045939, 0.224925786257, 0.221840232611])
+mean_std = ([0.446139603853, 0.409515678883, 0.395083993673], [0.288205742836, 0.278144598007, 0.283502370119])
 img_transform = standard_transforms.Compose([
         standard_transforms.ToTensor(),
         standard_transforms.Normalize(*mean_std)
@@ -28,99 +31,83 @@ restore = standard_transforms.Compose([
         standard_transforms.ToPILImage()
     ])
 pil_to_tensor = standard_transforms.ToTensor()
-
 LOG_PARA = 100.0
 
-dataRoot = '../ProcessedData/NWPU/XXX/'
 
-model_path = 'xxx.pth'
+dataRoot = '/media/E/own_open_source/ProcessedData/Data.2019.11/NWPU/1204_min_576x768_mod16_2048'
+model_path = '/media/E/linwei/CC/NEWC3F/exp/12-27_13-59_NWPU_VGG_1e-05_[01]/all_ep_16_mae_16.8_mse_27.8_nae_0.395.pth'
+
 
 def main():
-
-    with open(dataRoot+'test.txt') as f:
+    txtpath = os.path.join(dataRoot, 'txt_list', 'test.txt')
+    with open(txtpath) as f:
         lines = f.readlines()                            
-
     test(lines, model_path)
    
 
 def test(file_list, model_path):
 
-    net = CrowdCounter(cfg.GPU_ID,'VGG') # gpus, model name
-    net.load_state_dict(torch.load(model_path))
+    net = CrowdCounter(cfg.GPU_ID, 'VGG')
     net.cuda()
+    net.load_state_dict(torch.load(model_path))
     net.eval()
 
     gts = []
     preds = []
-    for infos in lines:
-        filename = info[0]
-    	print( filename )
+    f = open(f'submmited.txt', 'w+')
 
-        imgname = dataRoot + '/img/' + filename + '.jpg'
-
+    for infos in file_list:
+        filename, illum, level = infos.split()
+        
+        imgname = os.path.join(dataRoot, 'img', filename + '.jpg')
         img = Image.open(imgname)
         if img.mode == 'L':
             img = img.convert('RGB')
-        img = img_transform(img)
-
+        img = img_transform(img)[None, :, :, :]
         with torch.no_grad():
             img = Variable(img).cuda()
-            dot_map = Variable(dot_map).cuda()
-
-            crop_imgs, crop_dots, crop_masks = [], [], []
+            crop_imgs, crop_masks = [], []
             b, c, h, w = img.shape
-            rh, rw = self.cfg_data.TRAIN_SIZE
+            rh, rw = 576, 768
             for i in range(0, h, rh):
                 gis, gie = max(min(h-rh, i), 0), min(h, i+rh)
                 for j in range(0, w, rw):
                     gjs, gje = max(min(w-rw, j), 0), min(w, j+rw)
                     crop_imgs.append(img[:, :, gis:gie, gjs:gje])
-                    crop_dots.append(dot_map[:, :, gis:gie, gjs:gje])
-                    mask = torch.zeros_like(dot_map).cuda()
+                    mask = torch.zeros(b, 1, h, w).cuda()
                     mask[:, :, gis:gie, gjs:gje].fill_(1.0)
                     crop_masks.append(mask)
-            crop_imgs, crop_dots, crop_masks = map(lambda x: torch.cat(x, dim=0), (crop_imgs, crop_dots, crop_masks))
+            crop_imgs, crop_masks = map(lambda x: torch.cat(x, dim=0), (crop_imgs, crop_masks))
 
             # forward may need repeatng
-            crop_preds, crop_dens = [], []
-            nz, bz = crop_imgs.size(0), self.cfg_data.TRAIN_BATCH_SIZE
+            crop_preds = []
+            nz, bz = crop_imgs.size(0), 1
             for i in range(0, nz, bz):
                 gs, gt = i, min(nz, i+bz)
-                crop_pred, crop_den = self.net.forward(crop_imgs[gs:gt], crop_dots[gs:gt])
+                crop_pred = net.test_forward(crop_imgs[gs:gt])
                 crop_preds.append(crop_pred)
-                crop_dens.append(crop_den)
             crop_preds = torch.cat(crop_preds, dim=0)
-            crop_dens = torch.cat(crop_dens, dim=0)
 
             # splice them to the original size
             idx = 0
-            pred_map = torch.zeros_like(dot_map).cuda()
-            den_map = torch.zeros_like(dot_map).cuda()
+            pred_map = torch.zeros(b, 1, h, w).cuda()
             for i in range(0, h, rh):
                 gis, gie = max(min(h-rh, i), 0), min(h, i+rh)
                 for j in range(0, w, rw):
                     gjs, gje = max(min(w-rw, j), 0), min(w, j+rw)
                     pred_map[:, :, gis:gie, gjs:gje] += crop_preds[idx]
-                    den_map[:, :, gis:gie, gjs:gje] += crop_dens[idx]
                     idx += 1
 
             # for the overlapping area, compute average value
             mask = crop_masks.sum(dim=0).unsqueeze(0)
             pred_map = pred_map / mask
-            den_map = den_map / mask
-
         pred_map = pred_map.cpu().data.numpy()[0,0,:,:]
-
 
         pred = np.sum(pred_map) / LOG_PARA
 
-        with open('submmited.txt', 'a') as f:
-            f.write(filename_no_ext + ' %.4f\n' % (pred))
-                
-
+        print(f'{filename} {pred:.4f}', file=f)
+        print(f'{filename} {pred:.4f}')
+    f.close()
+            
 if __name__ == '__main__':
     main()
-
-
-
-
